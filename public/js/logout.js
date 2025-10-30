e0542b5ca7c3e8da2adb2a96d67fa06732cf6f29
ajay <ajaymevda999@gmail.com>
2025-10-30 19:50:16 +0530

    logout fix

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,6 +1,17 @@
-const logoutBtn = document.getElementById("logout-btn");
-  logoutBtn.addEventListener("click", () => {
-    localStorage.removeItem("user");
-    // alert("You have been logged out!");
-    window.location.href = "/";
-  });
+// const logoutBtn = document.getElementById("logout-btn");
+//   logoutBtn.addEventListener("click", () => {
+//     localStorage.removeItem("user");
+//     // alert("You have been logged out!");
+//     window.location.href = "/";
+//   });
+document.addEventListener("DOMContentLoaded", () => {
+  const logoutBtn = document.getElementById("logout-btn");
+
+  if (logoutBtn) {
+    logoutBtn.addEventListener("click", (e) => {
+      e.preventDefault(); // stops <a href="#"> from refreshing
+      localStorage.removeItem("user");
+      window.location.replace("/");
+    });
+  }
+});
