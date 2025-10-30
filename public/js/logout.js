6655eea29aa26b811251d2382322977da2660d7b
ajay <ajaymevda999@gmail.com>
2025-10-30 19:29:17 +0530

    Logout Changes

--- public/js/logout.js
+++ public/js/logout.js
@@ -2,5 +2,5 @@ const logoutBtn = document.getElementById("logout-btn");
   logoutBtn.addEventListener("click", () => {
     localStorage.removeItem("user");
     // alert("You have been logged out!");
-    window.location.href = "/login";
+    window.location.href = "/";
   });
